e9cf614cc3348af8cf849c79fee2e0a705d4bb91
Amed Reynaldo <elperro689@gmail.com>
2016-09-26 01:35:24 -0400

    quinto commit

--- js/javascrip.js
+++ js/javascrip.js
@@ -3,14 +3,35 @@ function multiplicar()
 	var num1=Number(document.getElementById('n1').value);
 	var num2=Number(document.getElementById('n2').value);
 	var res=num1*num2;
+	/*alert(res);*/
 	document.getElementById('res').value=res;
 }
 function aleatorio()
 {
-	var inferior=4;
+	var inferior=1;
 	var superir=100;
 	numPosibilidades=superir-inferior;
 	aleat=Math.random()*numPosibilidades;
 	aleat=Math.round(aleat);
 	alert(aleat);
+}
+document.getElementById("color").onchange = function(event){
+    var valor = event.target.value;
+    var nuevoColor = "linear-gradient("+valor+", #fff)";
+    document.getElementsByTagName("body")[0].style.background = nuevoColor;
+}
+document.getElementById("calcular").onclick = function(){
+    var N = document.getElementById("numero").value;
+    var lista = document.createElement("ul");
+    for(var i=1;i<=N;i++){
+        var item = document.createElement("li");
+        item.innerHTML = parseInt(Math.random()*100)+1;
+        lista.appendChild(item);
+    }
+    document.getElementsByTagName("body")[0].appendChild(lista);
+    return false;
+}
+function eliminar(event){
+    var padre = document.getElementsByTagName(".formulario2.ul")[0];
+    padre.removeChild(event.target);
 }
